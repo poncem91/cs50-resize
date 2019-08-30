14e003de93f391a309f07472e61ab4ed90f843e8
Mariafe Ponce <mafe.ponce@gmail.com>
2019-08-30 16:01:49 -0600

    Add files via upload

---
+++ resize.c
@@ -0,0 +1,156 @@
+// Resizes a BMP file
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // ensure proper usage
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: copy infile outfile\n");
+        return 1;
+    }
+
+    // remember filenames
+    char *infile = argv[2];
+    char *outfile = argv[3];
+
+    // remember n resize value
+    int n = atoi(argv[1]);
+
+    // ensure proper n value
+    if (n > 100 || n < 1)
+    {
+        fprintf(stderr, "Usage: value of n outside of parameteres\n");
+        return 1;
+    }
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER and create a copy for outfile
+    BITMAPFILEHEADER bf, bf2;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+    bf2 = bf;
+
+    // read infile's BITMAPINFOHEADER and create a copy for outfile
+    BITMAPINFOHEADER bi, bi2;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    bi2 = bi;
+
+    // adjust header info for outfile
+    bi2.biWidth *= n;
+    bi2.biHeight *= n;
+
+    // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int outpadding = (4 - ((bi2.biWidth) * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // adjust header info for outfile
+    bi2.biSizeImage = ((sizeof(RGBTRIPLE) * bi2.biWidth) + outpadding ) * abs(bi2.biHeight);
+    bf2.bfSize = bi2.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&bf2, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&bi2, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        //repeats process n-1 times
+        for (int l = 1; l < n; l++)
+        {
+
+        // iterate over pixels in scanline
+            for (int j = 0; j < bi.biWidth; j++)
+            {
+                // temporary storage
+                RGBTRIPLE triple;
+
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+                // write RGB triple to outfile
+                for (int p = 0; p < n; p++)
+                {
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
+
+            }
+
+            // add padding
+            for (int k = 0; k < outpadding; k++)
+            {
+                fputc(0x00, outptr);
+            }
+
+            // go back to beginning of scanline
+            fseek(inptr, -bi.biWidth * sizeof(RGBTRIPLE), SEEK_CUR);
+        }
+
+        // one more time through scanline
+        for (int j = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
+
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+            // write RGB triple to outfile
+            for (int p = 0; p < n; p++)
+            {
+                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            }
+        }
+
+        // add padding
+        for (int k = 0; k < outpadding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+
+        // skip over padding, if any
+        fseek(inptr, padding, SEEK_CUR);
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // success
+    return 0;
+}
